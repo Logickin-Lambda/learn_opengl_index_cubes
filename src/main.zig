6228939de43fe35d2c31b2fb506c4994ddb79d12
Logickin-Lambda <logickinlambda@protonmail.com>
2025-06-03 15:17:57 +0800

    - Code can now compiled, but with bugs The sb7 has been ported into zig with free of compile error, but there are some issues on the window creation process, resulting in some error.
    The next commit will address this issue so that
    I can generate a blank window.

--- src/main.zig
+++ src/main.zig
@@ -1,20 +1,10 @@
 // These are the libraries used in the examples,
 // you may find the respostories from build.zig.zon
-const glfw = @import("zglfw");
-const gl = @import("gl");
 const std = @import("std");
 const app = @import("application.zig");
 
 pub fn main() !void {
-    std.debug.print("<!--Skri-a Kaark-->", .{});
-    try app.init_default();
+    std.debug.print("<!--Skri-a Kaark-->\n", .{});
 
-    app.render = render;
-    try app.render(69);
-
-    std.debug.print("\nwindow name {s}\n", .{app.info.title});
-}
-
-fn render(input: f64) anyerror!void {
-    std.debug.print("\n{d} 420\n", .{input});
+    app.run();
 }
